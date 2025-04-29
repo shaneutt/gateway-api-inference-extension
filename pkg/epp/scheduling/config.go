53444a845289f919005ef391da63696c901a8207
Maya Barnea <mayab@il.ibm.com>
2025-04-29 09:39:03 +0300

    Add load based scorer (#77)
    * Added scorer based on pod's load (length of waiting requests queue)
    
    * Added scorer based on pod's load (length of waiting requests queue)
    
    * Update load based scorer according to scorer API changes
    
    * Fixes according to the PR comments
    
    Fix #66

--- pkg/epp/scheduling/config.go
+++ pkg/epp/scheduling/config.go
@@ -16,7 +16,10 @@ limitations under the License.
 
 package scheduling
 
-import "sigs.k8s.io/gateway-api-inference-extension/pkg/epp/scheduling/plugins"
+import (
+	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/scheduling/plugins"
+	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/scheduling/plugins/scorers"
+)
 
 type SchedulerConfig struct {
 	preSchedulePlugins  []plugins.PreSchedule
@@ -32,10 +35,18 @@ var defPlugin = &defaultPlugin{}
 // it's possible to call NewSchedulerWithConfig to pass a different argument.
 
 // For build time plugins changes, it's recommended to change the defaultConfig variable in this file.
-var defaultConfig = &SchedulerConfig{
-	preSchedulePlugins:  []plugins.PreSchedule{},
-	filters:             []plugins.Filter{defPlugin},
-	scorers:             map[plugins.Scorer]int{},
-	picker:              defPlugin,
-	postSchedulePlugins: []plugins.PostSchedule{},
+var defaultConfig = createDefaultConfig()
+
+func createDefaultConfig() *SchedulerConfig {
+	defConfig := &SchedulerConfig{
+		preSchedulePlugins:  []plugins.PreSchedule{},
+		filters:             []plugins.Filter{defPlugin},
+		scorers:             map[plugins.Scorer]int{},
+		picker:              defPlugin,
+		postSchedulePlugins: []plugins.PostSchedule{},
+	}
+
+	defConfig.scorers[scorers.NewLoadBasedScorer()] = 1.0
+
+	return defConfig
 }
