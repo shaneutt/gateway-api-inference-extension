c56372cd2400fa1763c035a9689617ff5eff237d
Andrew Anderson <andy@clubanderson.com>
2025-04-30 23:42:01 -0400

    fix lint

--- pkg/epp/server/runserver_test.go
+++ pkg/epp/server/runserver_test.go
@@ -18,7 +18,7 @@ package server_test
 import (
 	"testing"
 
-	. "sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
 
 	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/server"
 	logutil "sigs.k8s.io/gateway-api-inference-extension/pkg/epp/util/logging"
@@ -27,7 +27,7 @@ import (
 func TestRunnable(t *testing.T) {
 	// Make sure AsRunnable() does not use leader election.
 	runner := server.NewDefaultExtProcServerRunner().AsRunnable(logutil.NewTestLogger())
-	r, ok := runner.(LeaderElectionRunnable)
+	r, ok := runner.(manager.LeaderElectionRunnable)
 	if !ok {
 		t.Fatal("runner is not LeaderElectionRunnable")
 	}
